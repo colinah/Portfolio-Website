bfafc6403e93a0216ed259b2de8202b687d489e8
Colin Hale <colinah@colins-mbp.lan>
2019-12-17 13:21:49 -0700

    Updating footer tagline

--- portfolio-website/src/containers/Footer/Footer.js
+++ portfolio-website/src/containers/Footer/Footer.js
@@ -3,7 +3,7 @@ import classes from './Footer.module.scss';
 const footer = () => {
     return (
         <div className={classes.FooterContainer}>
-            <p className={classes.Footer}>Footer</p>
+            <p className={classes.Footer}> Disciple of Learning to Love Learning</p>
         </div> 
     )
 }
