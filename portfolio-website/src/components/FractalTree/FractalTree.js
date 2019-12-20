8ebec5bf00b44aa4116c03cb7a6c758b411fcbea
Colin Hale <colinah@colins-mbp.lan>
2019-12-20 15:06:44 -0700

    improveing header formatting

---
+++ portfolio-website/src/components/FractalTree/FractalTree.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import Sketch from 'react-p5';
+
+function Branch (begin,end,p5) {
+    this.begin = begin;
+    this.end = end;
+    this.displayed = false;
+    this.Vector = [];
+
+    this.Vector.sub = function sub(v1, v2, target) {
+        if (!target) {
+          target = v1.copy();
+        } else {
+          target.set(v1);
+        }
+        target.sub(v2);
+        return target;
+      };
+
+      this.Vector.add = function add(v1, v2, target) {
+        if (!target) {
+          target = v1.copy();
+        } else {
+          target.set(v1);
+        }
+        target.add(v2);
+        return target;
+      };
+      this.Vector.mult = function mult(v, n, target) {
+        if (!target) {
+          target = v.copy();
+        } else {
+          target.set(v);
+        }
+        target.mult(n);
+        return target;
+      };
+
+
+
+    this.show = (p5) => {
+        p5.stroke(255);
+        p5.strokeWeight(2);
+        p5.line(this.begin.x, this.begin.y,this.end.x,this.end.y);
+    }
+
+    this.fork = (angle, scl , p5) => {
+        let vec = this.Vector.sub(this.end, this.begin);
+        vec.rotate(angle)
+        vec.mult(scl)
+        let newEnd = this.Vector.add(this.end,vec);
+        let newBranch = new Branch(this.end,newEnd)
+        this.finished = true;
+        return newBranch
+    }
+}
+
+class FractalTree extends Component {
+    state = {
+        width: null,
+        height: null,
+    }
+    tree = [];
+
+addBranchesHandler = () => {
+    for(let i = this.tree.length-1; i >= 0; i--){
+        if(!this.tree[i].finished){
+          this.tree.push(this.tree[i].fork(this.props.angle,this.props.scl));
+          this.tree.push(this.tree[i].fork(-this.props.angle,this.props.scl))
+        }
+      }
+}
+
+setup = (p5, parent) => {
+    p5.createCanvas(this.props.width,this.props.height).parent(parent);
+    let a = p5.createVector(this.props.startLocation[0].x,this.props.startLocation[0].y);
+    let b = p5.createVector(this.props.startLocation[1].x,this.props.startLocation[1].y);
+    this.tree[0] = new Branch(a,b,p5)
+    p5.frameRate(1)
+}
+ 
+draw = p5 => {
+    for(let i = 0; i < this.tree.length;i++){
+        this.tree[i].show(p5)
+      }
+      if(this.tree.length < 20){
+        this.addBranchesHandler()
+      }
+}
+
+
+render() {
+    return (
+        <div>
+            <Sketch setup={this.setup} draw={this.draw} mousePressed={this.addBranchesHandler}/>
+            <p>width:{this.props.width}</p>
+        </div>
+    )
+  }
+}
+
+export default FractalTree
--- portfolio-website/src/containers/Header/Header.js
+++ portfolio-website/src/containers/Header/Header.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Header.module.scss';
-const header = () => {
+import FractalTree from '../../components/FractalTree/FractalTree';
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { width: 0,
+                        height: 0,
+                        scl: 0.8,
+                        angle: 3.14159/8,
+                        shouldUpdate: false};
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+      }
+      
+      componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+      }
+      
+      componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+      }
+      
+      updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight});
+      }
+
+
+
+    render(){
+        let sketch = (<FractalTree 
+            startLocation = {[{x:this.state.width/2,y:this.state.height},{x:this.state.width/2,y:this.state.height-(this.state.height/4)}]}
+            width = {this.state.width}
+            height = {this.state.height}
+            scl = {this.state.scl}
+            angle = {this.state.angle}
+            />)
     return (
         <div className={classes.ImageContainer}>
+            <div className={classes.TreeContainer }> 
+                {sketch}
+            </div>
             <div className={classes.TitleContainer}>
                 <h2 className = {classes.Intro}>Hello, I'm</h2>
                 <h1 className = {classes.Name} >Colin Hale</h1>
@@ -10,7 +49,8 @@ const header = () => {
             </div>
         </div>
     )
+    }
 }
 
-export default header
+export default Header
 
